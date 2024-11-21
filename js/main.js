0843278c94d4532c9b36cfbd9d654f445efcb05e
Summerpepper2023 <davidfgonzalezc1@gmail.com>
2024-11-20 19:01:53 -0500

    Maquetacion Terminada

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-// Se inicializa la libreria de animaciones AOS
-AOS.init()
+document.addEventListener('DOMContentLoaded', function(){
+    // Se inicializa la libreria de animaciones AOS
+    AOS.init()
+
+    // Imagenes experiencia
+    const tarjetasExperiencia = document.querySelectorAll('#experience .experience-card')
+    
+    tarjetasExperiencia.forEach(tarjeta =>
+        
+        tarjeta.addEventListener('mouseenter', function(){
+            // Obtenemos el ID de cada imagen
+            const imagenId = tarjeta.querySelector('h5').innerText.toLocaleLowerCase()
+            const imagenes = document.querySelectorAll('#experience img')
+
+            // Quitamos la clase enabled a todas las imagenes
+            imagenes.forEach(imagen =>
+                imagen.classList.remove('enabled')
+            )
+
+            const imagenSeleccionada = document.getElementById(imagenId)
+            imagenSeleccionada.classList.add('enabled')
+        })
+    )
+
+})
